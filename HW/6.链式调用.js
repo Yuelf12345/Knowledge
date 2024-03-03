eb270f4eaf6be4288554a3c61a48773efefd0c8a
yue <2794127100@qq.com>
2024-03-03 22:34:33 +0800

    flat

--- HW/6.链式调用.js
+++ HW/6.链式调用.js
@@ -1,4 +1,23 @@
-var arr = [1, 2, 3, 4];
-var obj = { };
-[ ].push.apply(obj, arr);
-console.log(obj);
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    setName(name) {
+        this.name = name;
+        return this;
+    }
+
+    setAge(age) {
+        this.age = age;
+        return this;
+    }
+
+    showInfo() {
+        console.log(`姓名：${this.name},年龄：${this.age}`);
+    }
+}
+
+let p = new Person().setName('张三').setAge(18);
+p.showInfo();
---
+++ JS/6.类型判断.js
@@ -0,0 +1,12 @@
+console.log(typeof 1);
+console.log(typeof '');
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof null);
+console.log(typeof Symbol('1'));
+console.log('=====instanceof====');
+console.log([] instanceof Array)
+console.log({} instanceof Object)
+console.log(null instanceof Object)
