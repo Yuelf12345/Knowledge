8138cdb45b9b8f3466391e6a3f93f580c5d5a5d0
yue <2794127100@qq.com>
2024-03-04 20:43:07 +0800

    vue3 api

--- 1.js
+++ 1.js
@@ -1,19 +1,26 @@
-const flat = (arr, depth) => {
-    let res = [];
-    helper = (arr, depth = 1) => {
-        for (let i = 0; i < arr.length; i++) {
-            if (Array.isArray(arr[i]) && depth > 0) {
-                helper(arr[i], depth - 1);
-            } else {
-                res.push(arr[i]);
-            }
-        }
-    }
-    helper(arr, depth)
-    return res;
+function add(x) {
+    let sum = x;
+
+    const innerAdd = function (y) {
+        sum += y;
+        // 返回innerAdd函数自身以支持连续调用
+        return innerAdd;
+    };
+
+    // 添加一个终止方法或者检查是否有传入参数来返回最终结果
+    innerAdd.toString = function () {
+        // 当调用toString时（如console.log()）返回计算后的结果
+        return sum;
+    };
+
+    // 当没有传入参数时，返回累计的结果
+    innerAdd.valueOf = function () {
+        return sum;
+    };
+
+    // 在函数内部返回innerAdd函数
+    return innerAdd;
 }
 
-
-
-console.log(flat([1, [2, [3, [4]]]], 2));
-console.log(flat([1, [2, [3, [4]]]], 4));
+// 连续调用并获取结果
+console.log(add(1)(2)(3));  // 输出6
