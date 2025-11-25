c37d3c55bcb3afe3673d898e891f5767997002d3
Yuelf <2794127100@qq.com>
2025-11-25 22:38:25 +0800

    feat: 无重复子串

--- LeetCode/3.无重复字符的最长子串.ts
+++ LeetCode/3.无重复字符的最长子串.ts
@@ -1,13 +1,15 @@
 function lengthOfLongestSubstring(s: string): number {
     const map = new Map()
     let maxLen = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (map.has(s[i])) {
-            maxLen = Math.max(maxLen, (i - map.get(s[i])))
+    let l = 0;
+    for (let r = 0; r < s.length; r++) {
+        if (map.has(s[r]) && map.get(s[r]) >= l) {
+            l = map.get(s[r]) + 1
         }
-        map.set(s[i], i)
+        map.set(s[r], r)
+        maxLen = Math.max(maxLen, r - l + 1)
     }
     return maxLen
 };
 
-console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring(" "));
